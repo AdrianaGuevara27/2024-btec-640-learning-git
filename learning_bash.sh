edd89d9b6db76d05c0a6f0caf781288d7bf44641
Adriana Guevara <amguevara3@dons.usfca.edu>
2024-10-16 17:55:31 -0700

    Create bash script to run wc on files

---
+++ learning_bash.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# A script to learn about bash scripts and passing in parameters
+# from the command line
+
+# Adriana Guevara
+# October 16, 2024
+# amguevara3@dons.usfca.edu
+
+if [ $# -eq 0 ]
+then
+	echo "Please supply a single filename as an argument."
+	exit 1
+fi
+
+for file in $@
+do
+	echo "Counting lines, words, and characters in $file..."
+	wc $file
+done
+
